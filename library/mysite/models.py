3c04bc4792e68c57c99a640b45c0bcd3ff2d29a0
songchiping <11056009@ntub.edu.tw>
2023-11-01 17:14:30 +0800

    1101

--- library/mysite/models.py
+++ library/mysite/models.py
@@ -5,6 +5,7 @@ class Post(models.Model):
     title = models.CharField(max_length = 200)
     slug = models.CharField(max_length = 200)
     body = models.TextField()
+    release_date = models.TextField()
     pub_date = models.DateTimeField(auto_now_add=True)
     
     class Meta:
