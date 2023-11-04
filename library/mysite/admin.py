700f86585b2b1a72e9c903678bcb4f9d267179db
songchiping <11056009@ntub.edu.tw>
2023-11-04 12:03:10 +0800

    1104

--- library/mysite/admin.py
+++ library/mysite/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
-from mysite.models import Post
+from mysite.models import Post,Product
+# from mysite.models import NewTable
 # Register your models here.
 
 class PostAdmin(admin.ModelAdmin):
-    list_display=('title','slug','pub_date','release_date')
+    list_display=('title','slug', 'release_date')
     
 admin.site.register(Post,PostAdmin)
+admin.site.register(Product)
+# admin.site.register(Product)
